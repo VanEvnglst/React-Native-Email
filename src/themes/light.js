ea6defc26628bd5d0d92028f8f25576b29412f54
VanEvnglst <engr.ivan.evangelista@gmail.com>
2022-08-09 19:32:43 +0800

    create colors

---
+++ src/themes/light.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@shopify/restyle';
+
+const palette = {
+  black: 'black',
+  red: 'red',
+  blue: 'blue',
+  yellow: 'yellow',
+  paper00: '#ffffff',
+  paper10: '#f5f5f4',
+  paper20: '#e6e6e6',
+  paper300: '#767577',
+  paper900: '#202020',
+  blue70: '#2185d0',
+  navy20: '#171a21',
+  navy900: '#b9babc',
+}
