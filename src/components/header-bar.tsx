1b2a4abc84508e095ae17227a23185512a811db2
VanEvnglst <engr.ivan.evangelista@gmail.com>
2023-01-07 15:28:53 +0800

    adjustments to header bar

--- src/components/header-bar.tsx
+++ src/components/header-bar.tsx
@@ -4,7 +4,7 @@ import AnimatedBox, { AnimatedBoxProps } from '../atoms/animated-box';
 
 const HeaderBar: React.FC<AnimatedBoxProps> = ({ children, ...rest }) => {
   return (
-    <AnimatedBox position='absolute' top={40} left={0} right={0} {...rest}>
+    <AnimatedBox position='absolute' top={0} left={0} right={0} {...rest}>
       <Bar variant='headerBar' flexDirection='row' alignItems='center' mx='lg' my='md' px='sm' minHeight={44}>{children}</Bar>
     </AnimatedBox>
   )
