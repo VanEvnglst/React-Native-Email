d02c99839066041680f1792ffd5f6199f69df5b7
VanEvnglst <engr.ivan.evangelista@gmail.com>
2023-01-10 14:30:19 +0800

    implement status bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from '@shopify/restyle'
 import Navigations from './navigation';
 import light from './themes/light';
-
+import StatusBar from './components/status-bar';
 const App = () => {
   return (
     <NavigationContainer>
       <ThemeProvider theme={light}>
+        <StatusBar />
      <Navigations/>
      </ThemeProvider>
     </NavigationContainer>
