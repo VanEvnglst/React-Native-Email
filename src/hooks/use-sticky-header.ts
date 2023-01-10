1267d44ecab6b4f7faf165423b9439c943ee6659
VanEvnglst <engr.ivan.evangelista@gmail.com>
2023-01-10 11:36:39 +0800

    handle end drag for sticky header

--- src/hooks/use-sticky-header.ts
+++ src/hooks/use-sticky-header.ts
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
-import { LayoutChangeEvent } from "react-native";
-import { event, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import { LayoutChangeEvent, NativeScrollEvent } from "react-native";
+import { event, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const ANCHOR_INIT = -9999
@@ -19,6 +19,14 @@ export default function useStickyHeader() {
     setHeaderBarHeight(event.nativeEvent.layout.height) 
   }, []);
 
+  const handleEndDrag = (event: NativeScrollEvent) => {
+    'worklet'
+    if (progressY.value > 0.5 || event.contentOffset.y < headerBarHeight) {
+      translationY.value = withTiming(maxY)
+    } else {
+      translationY.value = withTiming(minY)
+    }
+  }
   const handleScroll = useAnimatedScrollHandler({
     onBeginDrag: event => {
       anchorY.value = event.contentOffset.y
@@ -34,7 +42,9 @@ export default function useStickyHeader() {
         translationY.value = value;
         anchorY.value = offsetY
         progressY.value = interpolate(translationY.value, [minY, maxY], [0,1])
-    }
+    },
+    onEndDrag: handleEndDrag,
+    onMomentumEnd: handleEndDrag
   }, [minY, maxY, headerBarHeight])
 
   const headerBarStyle = useAnimatedStyle(() => ({
