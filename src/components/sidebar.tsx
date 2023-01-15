b60ca7ea44451196df9e4a52a2b278667972dd17
VanEvnglst <engr.ivan.evangelista@gmail.com>
2023-01-15 21:51:49 +0800

    use book list

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View, SafeAreaView } from 'react-native';
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
 import { Box,Text  } from '../atoms';
+import BookList from './book-list';
 
-const Sidebar: React.FC<DrawerContentComponentProps> = () => {
+const Sidebar: React.FC<DrawerContentComponentProps> = ({ navigation }) => {
+  const handleBookListItemPress = useCallback(() => {
+    navigation.closeDrawer();
+  }, [navigation]);
+
 return (
  <Box flex={1} bg='$sidebarBackground'>
   <SafeAreaView>
     <Text variant='sidebar' m='lg'>Inkdrop</Text>
   </SafeAreaView>
+    <BookList onPressItem={handleBookListItemPress}
+    />
  </Box>
 )
 }
